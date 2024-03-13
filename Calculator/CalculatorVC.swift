87c3eac9dd350217b97d9eaa90cda13b8f26b6b4
hinakkograshi <hinaf0224@gmail.com>
2024-03-14 06:54:26 +0900

    add: SampleComplete

--- Calculator/CalculatorVC.swift
+++ Calculator/CalculatorVC.swift
@@ -11,30 +11,30 @@ import Combine
 import CombineCocoa
 
 class CalculatorVC: UIViewController {
-
+    
     private let logoView = LogoView()
     private let resultView = ResultView()
     private let billInputView = BillInputView()
     private let tipInputView = TipInputView()
     private let splitInputView = SplitInputView()
-
+    
     private lazy var vStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
-        logoView,
-        resultView,
-        billInputView,
-        tipInputView,
-        splitInputView,
-        UIView()
+            logoView,
+            resultView,
+            billInputView,
+            tipInputView,
+            splitInputView,
+            UIView()
         ])
         stackView.axis = .vertical
         stackView.spacing = 36
         return stackView
     }()
-
+    
     private let vm = CalculatorVM()
     private var cancellables = Set<AnyCancellable>()
-
+    
     //キーボード外押したらキーボード閉じる
     private lazy var viewTapPublisher: AnyPublisher<Void, Never> = {
         let tapGesture = UITapGestureRecognizer(target: self, action: nil)
@@ -43,7 +43,7 @@ class CalculatorVC: UIViewController {
             Just(())
         }.eraseToAnyPublisher()
     }()
-//計算結果をリセット
+    //計算結果をリセット
     private lazy var logoViewTapPublisher: AnyPublisher<Void, Never> = {
         let tapGesture = UITapGestureRecognizer(target: self, action: nil)
         tapGesture.numberOfTapsRequired = 2
@@ -52,24 +52,24 @@ class CalculatorVC: UIViewController {
             Just(())
         }.eraseToAnyPublisher()
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
         bind()
         observe()
     }
-
+    
     private func observe() {
         viewTapPublisher.sink { [unowned self] value in
             view.endEditing(true)
         }.store(in: &cancellables)
-
-//        logoViewTapPublisher.sink { _ in
-//            print("Logo")
-//        }.store(in: &cancellables)
+        
+        //        logoViewTapPublisher.sink { _ in
+        //            print("Logo")
+        //        }.store(in: &cancellables)
     }
-
+    
     private func bind() {
         let input = CalculatorVM.Input(
             billPublisher: billInputView.valuePublisher,
@@ -77,16 +77,16 @@ class CalculatorVC: UIViewController {
             splitPublisher: splitInputView.valuePublisher,
             logoViewTapPublisher: logoViewTapPublisher)
         let output = vm.transform(input: input)
-
+        
         output.updateViewPublisher.sink { [unowned self] result in
             resultView.configure(result: result)
         }.store(in: &cancellables)
-
+        
         output.resetCalculatorPublisher.sink { [unowned self] _ in
             billInputView.reset()
             tipInputView.reset()
             splitInputView.reset()
-
+            
             UIView.animate(
                 withDuration: 0.1,
                 delay: 0,
@@ -100,10 +100,10 @@ class CalculatorVC: UIViewController {
                         self.logoView.transform = .identity
                     })
                 }
-
+            
         }.store(in: &cancellables)
     }
-
+    
     private func layout() {
         view.backgroundColor = ThemeColor.bg
         view.addSubview(vStackView)
@@ -113,29 +113,26 @@ class CalculatorVC: UIViewController {
             make.bottom.equalTo(view.snp.bottomMargin).offset(-16)
             make.top.equalTo(view.snp.topMargin).offset(16)
         }
-
+        
         logoView.snp.makeConstraints { make in
             make.height.equalTo(48)
         }
-
+        
         resultView.snp.makeConstraints { make in
             make.height.equalTo(224)
         }
-
+        
         billInputView.snp.makeConstraints { make in
             make.height.equalTo(56)
         }
-
+        
         tipInputView.snp.makeConstraints { make in
             make.height.equalTo(56+16+56)
         }
-
+        
         splitInputView.snp.makeConstraints { make in
             make.height.equalTo(56)
         }
-
+        
     }
-
-
 }
-
